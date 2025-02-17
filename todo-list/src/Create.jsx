25ad94d3d7425b37c9ba7dcd6f94620e1faeb343
David Ramovich Mandal <davidramovichmandal@gmail.com>
2025-02-18 01:28:03 +0545

    initial message

---
+++ todo-list/src/Create.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { type } from './../node_modules/axios/index.d';
+
+function Create() {
+  return (
+    <div>
+      <input type="text" name='' id='' />
+      <button type="button">
+        Add
+      </button>
+    </div>
+  )
+}
+
+export default Create
---
+++ todo-list/src/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <h2>
+        ToDo-List
+      </h2>
+    </div>
+  )
+}
+
+export default Home
