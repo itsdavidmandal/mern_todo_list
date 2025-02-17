f3734ff11d079f8982d04967adec42192e6279b5
David Ramovich Mandal <davidramovichmandal@gmail.com>
2025-02-18 01:48:43 +0545

    added usestate

--- todo-list/src/Home.jsx
+++ todo-list/src/Home.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Create from './Create'
 
 function Home() {
+    const [ todos, setTodos] = useState([])
   return (
     <div>
       <h2>
         ToDo-List
       </h2>
+      <Create/>
+      {
+        todos.map(todo=>(
+            <div>
+                {todo}
+            </div>
+        ))
+      }
     </div>
   )
 }
